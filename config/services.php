6f6472858ebe9e3171b87de813c3ddd51964e58e
MarkMurithi <murithimark@gmail.com>
2024-08-22 12:01:48 +0300

    Update

--- config/services.php
+++ config/services.php
@@ -2,17 +2,7 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
+    
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
@@ -30,18 +20,18 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'github' => [
-        'client_id' => '24fbd0edd46019058da7', //Github API
-        'client_secret' => '4500e6ab7a9bb70608600fbdee37969276bb0685', //Github Secret
+        'client_id' => '24fbd0edd46019058da7', 
+        'client_secret' => '4500e6ab7a9bb70608600fbdee37969276bb0685', 
         'redirect' => 'http://localhost:8000/login/github/callback',
      ],
      'google' => [
-        'client_id' => '122886334250-fsfd8ugugkr0hjrkakukbgnnruomjn95.apps.googleusercontent.com', //Google API
-        'client_secret' => 'eyuwbzXGBgbebzPXNiqSOods', //Google Secret
+        'client_id' => '122886334250-fsfd8ugugkr0hjrkakukbgnnruomjn95.apps.googleusercontent.com', 
+        'client_secret' => 'eyuwbzXGBgbebzPXNiqSOods', 
         'redirect' => 'http://localhost:8000/login/google/callback',
      ],
      'facebook' => [
-        'client_id' => '1494792524037846', //Facebook API
-        'client_secret' => '1bf13c2faf680f4ae2b97bdafa92c7d2', //Facebook Secret
+        'client_id' => '1494792524037846', 
+        'client_secret' => '1bf13c2faf680f4ae2b97bdafa92c7d2', 
         'redirect' => 'http://localhost:8000/login/facebook/callback',
      ],
 
